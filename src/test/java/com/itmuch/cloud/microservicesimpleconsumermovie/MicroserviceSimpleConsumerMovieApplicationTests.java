9b0a8273f8b407224611e963d1a3266fdb5f289e
china-zhz <zhanghengzeng1997@163.com>
2020-08-18 23:30:44 +0800

    服务消费者demo

---
+++ src/test/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.microservicesimpleconsumermovie;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSimpleConsumerMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
